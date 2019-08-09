10c6332e7dd02a4f0085197f4bac0abfed453c86
anoonymoost <anonymostgithub96@gmail.com>
2019-08-10 01:40:27 +0700

    up

---
+++ js/demo/datatables-demo.js
@@ -0,0 +1,12 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable();
+});
+
+
+// $(document).ready(function () {
+//   $('#dataTable').DataTable({
+//     "pagingType": "simple"
+//   });
+//   $('.dataTables_length').addClass('bs-select');
+// });
